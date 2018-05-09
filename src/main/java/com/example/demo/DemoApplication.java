befcf560522bbaf5c563de059f8ca70670ae156a
otanikotani <otanikotani@gmail.com>
2018-05-09 11:20:44 +0700

    Buildable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RestController
+	public class Ctrl {
+
+		@GetMapping("/hello")
+		public String test() throws UnknownHostException {
+			return "Hello from " + InetAddress.getLocalHost();
+		}
 	}
 }
